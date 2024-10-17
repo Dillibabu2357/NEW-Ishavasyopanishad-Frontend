3cbc298cd42c4bd97ff65c497d09277f1e4f060c
Pranav K Bhaskar <pkb19032002@gmail.com>
2024-10-17 09:59:04 +0530

    Refactor: Changed MantraView to SutraView and removed additional dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,16 @@ export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        armygreen: "#A77E20",
-      },
-    },
+  	extend: {
+  		colors: {
+  			armygreen: '#A77E20'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 }
